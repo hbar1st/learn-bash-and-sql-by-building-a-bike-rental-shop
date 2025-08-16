15f37b3dc610dc823f4fd3bc620389a1d3b62240
hbar1st <35541470+hbar1st@users.noreply.github.com>
2025-08-16 16:55:59 -0400

    db dump and start of bike-shop program

---
+++ bike-shop.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo -e "\n~~~~~ Bike Rental Shop ~~~~~\n"
+
+MAIN_MENU() 
+{
+  echo How may I help you?
+  echo -e "\n1. Rent a bike\n2. Return a bike\n3. Exit"
+
+  read MAIN_MENU_SELECTION
+}
+
+RENT_MENU() 
+{
+  echo Rent Menu
+}
+
+RETURN_MENU()
+{
+  echo Return Menu
+}
+
+EXIT()
+{
+  echo -e "\nThank you for stopping in.\n"
+}
+
+MAIN_MENU
