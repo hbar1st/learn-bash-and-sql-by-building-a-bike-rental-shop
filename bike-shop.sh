6b02b7536f6d578355736596636e9ebd81b29bfd
hbar1st <35541470+hbar1st@users.noreply.github.com>
2025-08-16 20:34:44 -0400

    Save progress: 1280.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -72,6 +72,8 @@ RENT_MENU()
         then
           # get new customer name
           echo -e "\nWhat's your name?"
+          read CUSTOMER_NAME
+          
           # insert new customer
         fi
       fi
