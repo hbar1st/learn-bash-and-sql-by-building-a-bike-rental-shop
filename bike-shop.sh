c91017b39e356f684729e568804b0549982b3d96
hbar1st <35541470+hbar1st@users.noreply.github.com>
2025-08-17 12:03:42 -0400

    Save progress: 1585.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -123,9 +123,9 @@ RETURN_MENU()
       # display rented bikes
       echo -e "\nHere are your rentals:"
       echo "$CUSTOMER_RENTALS" | while read BIKE_ID BAR TYPE BAR SIZE
-    do
-      echo "$BIKE_ID) $SIZE\" $TYPE Bike"
-    done
+      do
+        echo "$BIKE_ID) $SIZE\" $TYPE Bike"
+      done
       # ask for bike to return
       # if not a number
       # send to main menu
