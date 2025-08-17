6e4349465fb0359cf272a5d81c2245cd7958d481
hbar1st <35541470+hbar1st@users.noreply.github.com>
2025-08-17 11:18:54 -0400

    Save progress: 1530.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -112,7 +112,7 @@ RETURN_MENU()
     MAIN_MENU "I could not find a record for that phone number."
   else
     # get customer's rentals
-    
+    CUSTOMER_RENTALS=$($PSQL "select bike_id, type, size from bikes inner join rentals using(bike_id) inner join customers using(customer_id) where phone = '$PHONE_NUMBER' and date_returned is null order by bike_id")
     # if no rentals
     # send to main menu
   fi
